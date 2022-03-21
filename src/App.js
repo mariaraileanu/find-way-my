bf0750d0311cdc56fda5943c469c159fd836f4f6
raileanumaria <maria.raileanu@apptiq.co.uk>
2022-03-21 21:57:47 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Panel from './components/Panel';
+import "./App.css";
+import Panel from "./components/Panel";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootswatch/dist/slate/bootstrap.min.css";
 function App() {
   return (
-    <Panel />
+    <Router>
+      <Navbar />
+      <Panel />
+    </Router>
   );
 }
 
